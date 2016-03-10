b5a05b99ad79da7a0600aff209d4d1dbd263e79b
JingchengDu <jingcheng.du@intel.com>
2016-03-10 16:52:07 +0800

    Add back the read lock.

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java
@@ -178,10 +178,15 @@ class FsDatasetImpl implements FsDatasetSpi<FsVolumeImpl> {
 
   @Override
   public FsVolumeImpl getVolume(final ExtendedBlock b) {
-    Object lock = getBlockOpLock(b.getBlockId());
-    synchronized (lock) {
-      final ReplicaInfo r =  volumeMap.get(b.getBlockPoolId(), b.getLocalBlock());
-      return r != null? (FsVolumeImpl)r.getVolume(): null;
+    volumeOpLock.readLock().lock();
+    try {
+      Object lock = getBlockOpLock(b.getBlockId());
+      synchronized (lock) {
+        final ReplicaInfo r =  volumeMap.get(b.getBlockPoolId(), b.getLocalBlock());
+        return r != null? (FsVolumeImpl)r.getVolume(): null;
+      }
+    } finally {
+      volumeOpLock.readLock().unlock();
     }
   }
 
@@ -2518,10 +2523,15 @@ class FsDatasetImpl implements FsDatasetSpi<FsVolumeImpl> {
 
   @Override 
   public String getReplicaString(String bpid, long blockId) {
-    Object lock = getBlockOpLock(blockId);
-    synchronized (lock) {
-      final Replica r = volumeMap.get(bpid, blockId);
-      return r == null? "null": r.toString();
+    volumeOpLock.readLock().lock();
+    try {
+      Object lock = getBlockOpLock(blockId);
+      synchronized (lock) {
+        final Replica r = volumeMap.get(bpid, blockId);
+        return r == null? "null": r.toString();
+      }
+    } finally {
+      volumeOpLock.readLock().unlock();
     }
   }
 
@@ -2756,16 +2766,21 @@ class FsDatasetImpl implements FsDatasetSpi<FsVolumeImpl> {
   @Override // FsDatasetSpi
   public long getReplicaVisibleLength(final ExtendedBlock block)
   throws IOException {
-    Object lock = getBlockOpLock(block.getBlockId());
-    synchronized (lock) {
-      final Replica replica = getReplicaInfo(block.getBlockPoolId(), 
-        block.getBlockId());
-      if (replica.getGenerationStamp() < block.getGenerationStamp()) {
-        throw new IOException(
-            "replica.getGenerationStamp() < block.getGenerationStamp(), block="
-            + block + ", replica=" + replica);
+    volumeOpLock.readLock().lock();
+    try {
+      Object lock = getBlockOpLock(block.getBlockId());
+      synchronized (lock) {
+        final Replica replica = getReplicaInfo(block.getBlockPoolId(), 
+          block.getBlockId());
+        if (replica.getGenerationStamp() < block.getGenerationStamp()) {
+          throw new IOException(
+              "replica.getGenerationStamp() < block.getGenerationStamp(), block="
+              + block + ", replica=" + replica);
+        }
+        return replica.getVisibleLength();
       }
-      return replica.getVisibleLength();
+    } finally {
+      volumeOpLock.readLock().unlock();
     }
   }
 
--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/ReplicaMap.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/ReplicaMap.java
@@ -138,11 +138,9 @@ class ReplicaMap {
    * Add all entries from the given replica map into the local replica map.
    */
   void addAll(ReplicaMap other) {
-    synchronized (mutex) {
-      map.putAll(other.map);
-    }
+    map.putAll(other.map);
   }
-
+  
   /**
    * Remove the replica's meta information from the map that matches
    * the input block's id and generation stamp
